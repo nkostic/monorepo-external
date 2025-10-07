712fbffb02c5d7bf09c5ebd573903d2e522fe81a
Nenad Kostic <nenad.kostic@neofinancial.com>
2025-10-07 13:35:26 -0600

    clean the commit

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -12,7 +12,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
   return (
     <button
       className={className}
-      onClick={() => alert(`Hello from your ${appName} app! Extended by nkostic`)}
+      onClick={() => alert(`Hello from your ${appName} app!`)}
     >
       {children}
     </button>
